2f58a98d73ee077de98fbe8ae2483ce3f6472724
wAlienUFOx <47860127+wAlienUFOx@users.noreply.github.com>
2019-10-28 17:24:11 +0300

    Add files via upload

--- trapeze.cpp
+++ trapeze.cpp
@@ -5,37 +5,18 @@ Trapeze::Trapeze() : A{0, 0}, B{0, 0}, C{0, 0}, D{0,0} {}
 
 Trapeze::Trapeze(std::istream& in) {
   in >> A >> B >> C >> D;
-  double a, c;
-  a = sqrt((B.X()- A.X()) * (B.X() - A.X()) + (B.Y() - A.Y()) * (B.Y() - A.Y()));
-  c = sqrt((C.X()- D.X()) * (C.X() - D.X()) + (C.Y() - D.Y()) * (C.Y() - D.Y()));
-  if(a != c || (C.Y() - B.Y()) / (C.X() - B.X()) != (D.Y() - A.Y()) / (D.X() - A.X()))
-    throw std::logic_error("It`s not a isosceles trapeze");
+  if((C.Y() - B.Y()) / (C.X() - B.X()) != (D.Y() - A.Y()) / (D.X() - A.X()))
+    throw std::logic_error("It`s not a trapeze");
 }
 
 
 double Trapeze::area() const{
-  double a = sqrt((C.X() - B.X()) * (C.X() - B.X()) + (B.Y() - C.Y()) * (B.Y() - C.Y()));
-  double b = sqrt((B.X() - A.X()) * (B.X() - A.X()) + (B.Y() - A.Y()) * (B.Y() - A.Y()));
-  double l = sqrt((D.X() - A.X()) * (D.X() - A.X()) + (A.Y() - D.Y()) * (A.Y() - D.Y()));
-  double c = (l - a) / 2;
-  double h = sqrt((b * b) - (c * c));
-  return 0.5 * h * (a + l);
-	       
+  return 0.5 * abs( A.X() * B.Y() + B.X() * C.Y() + C.X() * D.Y() + D.X() * A.Y() - B.X() * A.Y() - C.X() * B.Y() - D.X() * C.Y() - A.X() * D.Y());	       
 }
 
 Point Trapeze::center() const
 {
-  double a = sqrt((C.X() - B.X()) * (C.X() - B.X()) + (B.Y() - C.Y()) * (B.Y() - C.Y()));
-  double b = sqrt((B.X() - A.X()) * (B.X() - A.X()) + (B.Y() - A.Y()) * (B.Y() - A.Y()));
-  double l = sqrt((D.X() - A.X()) * (D.X() - A.X()) + (A.Y() - D.Y()) * (A.Y() - D.Y()));
-  double c = (l - a) / 2;
-  double h = sqrt((b * b) - (c * c));
-  double y_ = (2 * l + a) * h / (a + l) / 3;
-  if (B.X() == C.X() && D.X() < C.X())
-    return Point{D.X() + h - y_, (A.Y() + B.Y() + C.Y() + D.Y()) / 4};
-  if (B.X() == C.X() && C.X() < D.X())
-    return Point{C.X() + h - y_, (A.Y() + B.Y() + C.Y() + D.Y()) / 4};
-  return Point{(A.X() + B.X() + C.X() + D.X()) / 4, (B.Y() + C.Y()) / 2 - ((B.Y() + C.Y()) / 2 - (D.Y() + A.Y()) / 2) * y_ / h};
+  return Point{(A.X() + B.X() + C.X() + D.X()) / 4, (A.Y() + B.Y() + C.Y() + D.Y()) / 4};
 }
 
 std::ostream& Trapeze::print(std::ostream& out) const
@@ -43,4 +24,3 @@ std::ostream& Trapeze::print(std::ostream& out) const
   out << A << " " << B << " " << C << " " << D;
   return out;
 }
-
